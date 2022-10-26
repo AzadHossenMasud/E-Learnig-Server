afba5cde34f210b0c4e85cab4fd0f5ee4c242031
Azad Hossen <azadhossen6677@gmail.com>
2022-10-26 08:42:46 +0600

    install cors

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 
@@ -28,11 +27,7 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
-app.get("/course/:id", (req, res) => {
-  const id = req.params.id;
-  const selectedCourse = courses.find((course) => course._id === id);
-  res.send(selectedCourse);
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
