07e081d39a69da61e122e328320d71288eaf25c8
Azad Hossen <azadhossen6677@gmail.com>
2022-10-26 08:44:13 +0600

    use cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 
 const catagories = require("./data/catagories.json");
 const courses = require("./data/courses.json");
+
+app.use(cors())
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -27,7 +31,11 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
-
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((course) => course._id === id);
+  res.send(selectedCourse);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
